7a2ce424c478e31dce64e18b8d46f1d885293deb
Dr. Derek Austin <derekraustin@gmail.com>
2022-08-12 21:08:16 -0500

    fix: link to DoctorDerek.com from footer

--- pages/index.js
+++ pages/index.js
@@ -130,14 +130,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.doctorderek.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          A DoctorDerek project
         </a>
       </footer>
     </div>
