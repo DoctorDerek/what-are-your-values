799bc90599189bf4410da7d9beae6c456e06fb03
Dr. Derek Austin <derekraustin@gmail.com>
2026-04-27 22:57:37 -0600

    chore(repomix): create repomix config file and `repomix-mapachito.bat`

---
+++ repomix-mapachito.bat
@@ -0,0 +1,19 @@
+@echo off
+chcp 65001 >nul
+
+call npx repomix
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+> repomix-temp.xml echo ================================================================================
+>> repomix-temp.xml echo COMPLETE REPOSITORY FILE LIST (FILTERED)
+>> repomix-temp.xml echo ================================================================================
+git ls-files -co --exclude-standard | findstr /V /I /E ".import .uid" >> repomix-temp.xml
+>> repomix-temp.xml echo ================================================================================
+>> repomix-temp.xml echo REPOMIX OUTPUT BEGINS BELOW:
+>> repomix-temp.xml echo ================================================================================
+>> repomix-temp.xml echo.
+type repomix-output.xml >> repomix-temp.xml
+
+move /y repomix-temp.xml repomix-output.xml >nul
+
+antigravity .\repomix-output.xml
