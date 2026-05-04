70b2eb52638e7a1803b3cff132721f49db23cfe5
Dr. Derek Austin <derekraustin@gmail.com>
2026-05-03 22:19:33 -0600

    fix(config): rename to correct shared package in `next.config.ts` option `transpilePackages`

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@wayvm/shared"],
+  transpilePackages: ["@what-are-your-values-mapache/shared"],
 }
 
 export default nextConfig
