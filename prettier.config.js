8369436fbc43f17c01bb200aab4283c23d79f99f
Dr. Derek Austin <derekraustin@gmail.com>
2026-05-03 20:43:09 -0600

    feat(migration): move app-specific config files into new monorepo specifically ESLint, Next.js, PostCSS, and TSConfig files

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: false,
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  tailwindStylesheet: "./styles/globals.css",
-  tailwindAttributes: ["tw"],
-  tailwindFunctions: ["tw", "classNames"],
-}
