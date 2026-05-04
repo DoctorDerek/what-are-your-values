e016c3fc13178f0ff74ef9bbf3adf34a9f99f07a
Dr. Derek Austin <derekraustin@gmail.com>
2026-05-03 21:14:27 -0600

    style(Prettier): run Prettier on repo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,8 +2,10 @@ import "./globals.css"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "What Are Your Values, Mapache? A Free Game To Find What You Value in Life",
-  description: "What Are Your Values, Mapache? is a fast-paced, value-sorting autobattler to help you find out what you value in life.",
+  title:
+    "What Are Your Values, Mapache? A Free Game To Find What You Value in Life",
+  description:
+    "What Are Your Values, Mapache? is a fast-paced, value-sorting autobattler to help you find out what you value in life.",
 }
 
 export default function RootLayout({
@@ -13,9 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen antialiased">
-        {children}
-      </body>
+      <body className="min-h-screen antialiased">{children}</body>
     </html>
   )
 }
