5b80476e2d69eae0e49b6ffad7f771b89d028aac
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-06 10:55:21 -0600

    chore(tooling): configure eslint warn-only and format codebase

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,15 @@
 import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
+import onlyWarn from "eslint-plugin-only-warn"
 
-const eslintConfig = [...nextConfig, eslintConfigPrettier]
+const eslintConfig = [
+  ...nextConfig,
+  {
+    plugins: {
+      "only-warn": onlyWarn,
+    },
+  },
+  eslintConfigPrettier,
+]
 
 export default eslintConfig
