8d7b9555ea19591751e3fe40794fedde9d424910
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-08 15:11:27 -0600

    chore(tooling): integrate eslint-config-flat-gitignore to dynamically ignore gitignored files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,10 @@
+import gitignore from "eslint-config-flat-gitignore"
 import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
 import onlyWarn from "eslint-plugin-only-warn"
 
 const eslintConfig = [
+  gitignore(),
   ...nextConfig,
   {
     plugins: {
