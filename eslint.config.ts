856b1a6374021a53d526d379d801b6ea98383487
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:12:05 -0600

    chore(tailwind): migrate postcss configuration into package.json

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,7 @@
 import gitignore from "eslint-config-flat-gitignore"
 import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
-import type { Linter } from "eslint"
+import type { ESLint, Linter } from "eslint"
 import onlyWarn from "eslint-plugin-only-warn"
 
 const eslintConfig: Linter.Config[] = [
@@ -9,7 +9,7 @@ const eslintConfig: Linter.Config[] = [
   ...nextConfig,
   {
     plugins: {
-      "only-warn": onlyWarn,
+      "only-warn": onlyWarn as unknown as ESLint.Plugin,
     },
   },
   eslintConfigPrettier,
