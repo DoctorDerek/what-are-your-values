592769260032790e9c7b75f687332e8be4d6e6aa
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 16:04:24 -0600

    chore(lint): add strict type annotations to eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,10 @@
 import gitignore from "eslint-config-flat-gitignore"
 import nextConfig from "eslint-config-next"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
+import type { Linter } from "eslint"
 import onlyWarn from "eslint-plugin-only-warn"
 
-const eslintConfig = [
+const eslintConfig: Linter.Config[] = [
   gitignore(),
   ...nextConfig,
   {
