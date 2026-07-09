92a6e0c738101b50ff16b5b4f64a9212a9eee8b4
Dr. Derek Austin <derekraustin@gmail.com>
2026-07-09 14:15:25 -0600

    refactor(config): rename package scope from @what-are-your-values-mapache to @game

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: [
-    "@what-are-your-values-mapache/data",
-    "@what-are-your-values-mapache/utils",
-    "@what-are-your-values-mapache/machines",
+    "@game/data",
+    "@game/utils",
+    "@game/machines",
   ],
 }
 
